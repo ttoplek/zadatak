821aad27ae66aed6f4ceea45b737c64ada62f734
ttoplek <toplektoni@gmail.com>
2019-04-08 09:04:27 +0200

    Zadatak: mjeri vrijeme pretipkavanja teksta

---
+++ Zadatak/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Zadatak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxRecenica = new System.Windows.Forms.TextBox();
+            this.textBoxPretipkavanja = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.buttonUnos = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxRecenica
+            // 
+            this.textBoxRecenica.Location = new System.Drawing.Point(212, 82);
+            this.textBoxRecenica.Name = "textBoxRecenica";
+            this.textBoxRecenica.Size = new System.Drawing.Size(219, 20);
+            this.textBoxRecenica.TabIndex = 0;
+            // 
+            // textBoxPretipkavanja
+            // 
+            this.textBoxPretipkavanja.Location = new System.Drawing.Point(212, 167);
+            this.textBoxPretipkavanja.Name = "textBoxPretipkavanja";
+            this.textBoxPretipkavanja.Size = new System.Drawing.Size(219, 20);
+            this.textBoxPretipkavanja.TabIndex = 1;
+            this.textBoxPretipkavanja.TextChanged += new System.EventHandler(this.textBoxPretipkavanja_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(471, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Unesi recenicu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pretipkaj recenicu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Vrijeme potrebno za pretipkavanje je:";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(396, 278);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(0, 13);
+            this.labelTime.TabIndex = 5;
+            // 
+            // buttonUnos
+            // 
+            this.buttonUnos.Location = new System.Drawing.Point(277, 123);
+            this.buttonUnos.Name = "buttonUnos";
+            this.buttonUnos.Size = new System.Drawing.Size(75, 23);
+            this.buttonUnos.TabIndex = 6;
+            this.buttonUnos.Text = "Unesi";
+            this.buttonUnos.UseVisualStyleBackColor = true;
+            this.buttonUnos.Click += new System.EventHandler(this.buttonUnos_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(388, 123);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonUnos);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPretipkavanja);
+            this.Controls.Add(this.textBoxRecenica);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRecenica;
+        private System.Windows.Forms.TextBox textBoxPretipkavanja;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button buttonUnos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Zadatak/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Diagnostics.Stopwatch sat = new System.Diagnostics.Stopwatch();
+        string recenica;
+        private void buttonUnos_Click(object sender, EventArgs e)
+        {
+            
+            recenica = textBoxRecenica.Text;
+
+        }
+        
+        private void textBoxPretipkavanja_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+            
+            if (recenica == textBoxPretipkavanja.Text)
+            {
+                sat.Stop();
+                textBoxPretipkavanja.Clear();
+                textBoxRecenica.Clear();
+                labelTime.Text = sat.Elapsed.TotalSeconds.ToString();
+
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            sat.Reset();
+            sat.Start();
+                      
+        }
+    }
+}
